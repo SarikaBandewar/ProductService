eb184ae1d950d7bbe5f8a3a262fab5984126b2f0
SarikaBandewar <shabs23119@yahoo.co.in>
2024-04-10 19:59:15 +0530

    db versioning init

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -4,76 +4,71 @@ CREATE TABLE category
     created_at datetime NULL,
     updated_at datetime NULL,
     title      VARCHAR(255) NULL,
-    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
-);
-
-CREATE TABLE category_seq
-(
-    next_val BIGINT NULL
+    CONSTRAINT pk_category PRIMARY KEY (id)
 );
 
 CREATE TABLE jt_instructor
 (
+    id       BIGINT NOT NULL,
     batch_id VARCHAR(255) NULL,
     subject  VARCHAR(255) NULL,
-    id       BIGINT NOT NULL,
-    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+    CONSTRAINT pk_jt_instructor PRIMARY KEY (id)
 );
 
 CREATE TABLE jt_student
 (
-    batch_id VARCHAR(255) NULL,
     id       BIGINT NOT NULL,
-    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+    batch_id VARCHAR(255) NULL,
+    CONSTRAINT pk_jt_student PRIMARY KEY (id)
 );
 
 CREATE TABLE jt_ta
 (
-    rating_per_session DOUBLE NULL,
-    session_id BIGINT NULL,
     id         BIGINT NOT NULL,
-    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+    session_id BIGINT NULL,
+    rating_per_session DOUBLE NULL,
+    CONSTRAINT pk_jt_ta PRIMARY KEY (id)
 );
 
 CREATE TABLE jt_user
 (
     id       BIGINT NOT NULL,
-    email_id VARCHAR(255) NULL,
     name     VARCHAR(255) NULL,
+    email_id VARCHAR(255) NULL,
     password VARCHAR(255) NULL,
-    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+    CONSTRAINT pk_jt_user PRIMARY KEY (id)
 );
 
 CREATE TABLE mp_instructor
 (
     id       BIGINT NOT NULL,
-    email_id VARCHAR(255) NULL,
     name     VARCHAR(255) NULL,
+    email_id VARCHAR(255) NULL,
     password VARCHAR(255) NULL,
     batch_id VARCHAR(255) NULL,
     subject  VARCHAR(255) NULL,
-    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+    CONSTRAINT pk_mp_instructor PRIMARY KEY (id)
 );
 
 CREATE TABLE mp_student
 (
     id       BIGINT NOT NULL,
-    email_id VARCHAR(255) NULL,
     name     VARCHAR(255) NULL,
+    email_id VARCHAR(255) NULL,
     password VARCHAR(255) NULL,
     batch_id VARCHAR(255) NULL,
-    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+    CONSTRAINT pk_mp_student PRIMARY KEY (id)
 );
 
 CREATE TABLE mp_ta
 (
     id         BIGINT NOT NULL,
-    email_id   VARCHAR(255) NULL,
     name       VARCHAR(255) NULL,
+    email_id   VARCHAR(255) NULL,
     password   VARCHAR(255) NULL,
-    rating_per_session DOUBLE NULL,
     session_id BIGINT NULL,
-    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+    rating_per_session DOUBLE NULL,
+    CONSTRAINT pk_mp_ta PRIMARY KEY (id)
 );
 
 CREATE TABLE product
@@ -81,84 +76,77 @@ CREATE TABLE product
     id            BIGINT NOT NULL,
     created_at    datetime NULL,
     updated_at    datetime NULL,
+    title         VARCHAR(255) NULL,
+    price DOUBLE NULL,
+    category_id   BIGINT NULL,
     `description` VARCHAR(255) NULL,
     image         VARCHAR(255) NULL,
-    price DOUBLE NULL,
-    title         VARCHAR(255) NULL,
-    category_id   BIGINT NULL,
-    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
-);
-
-CREATE TABLE product_seq
-(
-    next_val BIGINT NULL
+    CONSTRAINT pk_product PRIMARY KEY (id)
 );
 
 CREATE TABLE st_user
 (
-    user_type  INT    NOT NULL,
     id         BIGINT NOT NULL,
-    email_id   VARCHAR(255) NULL,
+    user_type  INT NULL,
     name       VARCHAR(255) NULL,
+    email_id   VARCHAR(255) NULL,
     password   VARCHAR(255) NULL,
     batch_id   VARCHAR(255) NULL,
     subject    VARCHAR(255) NULL,
-    rating_per_session DOUBLE NULL,
     session_id BIGINT NULL,
-    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+    rating_per_session DOUBLE NULL,
+    CONSTRAINT pk_st_user PRIMARY KEY (id)
 );
 
 CREATE TABLE tc_instructor
 (
     id       BIGINT NOT NULL,
-    email_id VARCHAR(255) NULL,
     name     VARCHAR(255) NULL,
+    email_id VARCHAR(255) NULL,
     password VARCHAR(255) NULL,
     batch_id VARCHAR(255) NULL,
     subject  VARCHAR(255) NULL,
-    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+    CONSTRAINT pk_tc_instructor PRIMARY KEY (id)
 );
 
 CREATE TABLE tc_student
 (
     id       BIGINT NOT NULL,
-    email_id VARCHAR(255) NULL,
     name     VARCHAR(255) NULL,
+    email_id VARCHAR(255) NULL,
     password VARCHAR(255) NULL,
     batch_id VARCHAR(255) NULL,
-    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+    CONSTRAINT pk_tc_student PRIMARY KEY (id)
 );
 
 CREATE TABLE tc_ta
 (
     id         BIGINT NOT NULL,
-    email_id   VARCHAR(255) NULL,
     name       VARCHAR(255) NULL,
+    email_id   VARCHAR(255) NULL,
     password   VARCHAR(255) NULL,
-    rating_per_session DOUBLE NULL,
     session_id BIGINT NULL,
-    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+    rating_per_session DOUBLE NULL,
+    CONSTRAINT pk_tc_ta PRIMARY KEY (id)
 );
 
 CREATE TABLE tc_user
 (
     id       BIGINT NOT NULL,
-    email_id VARCHAR(255) NULL,
     name     VARCHAR(255) NULL,
+    email_id VARCHAR(255) NULL,
     password VARCHAR(255) NULL,
-    CONSTRAINT `PRIMARY` PRIMARY KEY (id)
+    CONSTRAINT pk_tc_user PRIMARY KEY (id)
 );
 
-ALTER TABLE product
-    ADD CONSTRAINT FK1mtsbur82frn64de7balymq9s FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE NO ACTION;
-
-CREATE INDEX FK1mtsbur82frn64de7balymq9s ON product (category_id);
-
-ALTER TABLE jt_ta
-    ADD CONSTRAINT FK6vwcjedyvspg79nch4upoy2r7 FOREIGN KEY (id) REFERENCES jt_user (id) ON DELETE NO ACTION;
+ALTER TABLE jt_instructor
+    ADD CONSTRAINT FK_JT_INSTRUCTOR_ON_ID FOREIGN KEY (id) REFERENCES jt_user (id);
 
 ALTER TABLE jt_student
-    ADD CONSTRAINT FKcdd1n57kqnhoqnlgi7tpfhndy FOREIGN KEY (id) REFERENCES jt_user (id) ON DELETE NO ACTION;
+    ADD CONSTRAINT FK_JT_STUDENT_ON_ID FOREIGN KEY (id) REFERENCES jt_user (id);
 
-ALTER TABLE jt_instructor
-    ADD CONSTRAINT FKr77qonwnafpdy64wrhvjx7eyn FOREIGN KEY (id) REFERENCES jt_user (id) ON DELETE NO ACTION;
+ALTER TABLE jt_ta
+    ADD CONSTRAINT FK_JT_TA_ON_ID FOREIGN KEY (id) REFERENCES jt_user (id);
+
+ALTER TABLE product
+    ADD CONSTRAINT FK_PRODUCT_ON_CATEGORY FOREIGN KEY (category_id) REFERENCES category (id);
